14a74bccba4f7c3ba0e928be318e4091a0b6af2b
Ankur Joshi <ankurjoshi3012@gmail.com>
2023-02-02 17:41:32 +0530

    i want to see one more time got git commit works

--- add.py
+++ add.py
@@ -1,4 +1,5 @@
 a= int(input("enter the first number"))
 b=int(input("enter the secound number"))
 c= a+b
+print("so the answers of that qution is: ")
 print("the sum of the numbers are:",c)
--- sub.py
+++ sub.py
@@ -3,3 +3,4 @@ b=int(input("enter the secound number"))
 c= a-b
 #print("so the answers of that qution is: ")
 print("the sum of the numbers are:",c)
+print(" xkjlgbonrgw4nho otkhj l g5o3 oigh lfknb perkgnk  ")
